43d83b8e38af48fed2b90a5d9fd9c0c845f61a23
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-02-01 18:12:19 +0000

    Utility test

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -4,3 +4,19 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% NULL, NULL)
   expect_equal(NULL %||% 2, 2)
 })
+
+
+test_that("Can hash file", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp))
+
+  writeLines("hello world", tmp)
+  expected <- unname(tools::md5sum(tmp))
+  expect_equal(hash_file(tmp, "md5"), paste0("md5:", expected))
+  expect_equal(parse_hash(hash_file(tmp, "md5")),
+               list(algorithm = "md5", value = expected))
+
+  saveRDS(mtcars, tmp)
+  expected <- unname(tools::md5sum(tmp))
+  expect_equal(hash_file(tmp, "md5"), paste0("md5:", expected))
+})
