d65a63c43697538435afe99fe749412da3707a58
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-03-16 14:00:24 +0000

    More explicit testing

--- tests/testthat/test-location.R
+++ tests/testthat/test-location.R
@@ -1,3 +1,29 @@
+test_that("No locations except local by default", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+  root <- outpack_init(path)
+  expect_equal(outpack_location_list(root = path), "local")
+})
+
+
+test_that("Can add a location", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+
+  root <- list()
+  for (p in c("a", "b", "c")) {
+    fs::dir_create(file.path(path, p))
+    root[[p]] <- outpack_init(file.path(path, p))
+  }
+
+  outpack_location_add("b", root$b$path, root$a)
+  expect_setequal(outpack_location_list(root = root$a), c("local", "b"))
+
+  outpack_location_add("c", root$c$path, root$a)
+  expect_setequal(outpack_location_list(root = root$a), c("local", "b", "c"))
+})
+
+
 test_that("Can't add a location with reserved name", {
   path <- tempfile()
   on.exit(unlink(path, recursive = TRUE))
@@ -29,4 +55,22 @@ test_that("Can't add a location with existing name", {
     "A location with name 'upstream' already exists")
   expect_equal(outpack_location_list(root = root$a),
                c("local", "upstream"))
+})
+
+
+test_that("Require that (for now) locations must be paths", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+  root <- outpack_init(path)
+  expect_equal(outpack_location_list(root = path), "local")
+
+  other <- tempfile()
+  on.exit(unlink(other, recursive = TRUE), add = TRUE)
+  expect_error(
+    outpack_location_add("other", other, root = path),
+    "File does not exist:")
+  fs::dir_create(other)
+  expect_error(
+    outpack_location_add("other", other, root = path),
+    "'.+' does not look like an outpack root")
 })
