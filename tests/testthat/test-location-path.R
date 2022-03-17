acd27964ed29b60316a4077162f9d6da31022128
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-03-17 13:07:23 +0000

    Better tests

--- tests/testthat/test-location-path.R
+++ tests/testthat/test-location-path.R
@@ -1,94 +1,86 @@
-test_that("can pull metadata from a file base location", {
-  tmp <- tempfile()
-  on.exit(unlink(tmp, recursive = TRUE))
+test_that("can construct a outpack_location_path object", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
 
-  path_upstream <- file.path(tmp, "upstream")
-  root_upstream <- outpack_init(path_upstream, use_file_store = TRUE)
-
-  create_packet <- function(root) {
-    src <- tempfile()
-    on.exit(unlink(src, recursive = TRUE))
-    saveRDS(runif(10), file.path(src, "data.rds"))
-    id <- outpack_packet_start(src, "data", root = root)$id
-    outpack_packet_end()
-    id
-  }
-
-  ids <- vcapply(1:3, create_packet, file.path(tmp, "src"))
-
-  path_downstream <- file.path(tmp, "downstream")
-  outpack_init(path_downstream, use_file_store = TRUE)
-
-  outpack_location_add("upstream", path_upstream, root = path_downstream)
-  expect_equal(outpack_location_list(root = path_downstream),
-               c("local", "upstream"))
-
-  outpack_location_pull_metadata("upstream", root = path_downstream)
-
-  ## Sensible tests here will be much easier to write once we have a
-  ## decent query interface.
-
-  ## TODO: should not invalidate the existing open root we have, but
-  ## we do somehow.
-  index <- outpack_root_open(path_downstream)$index_update()
-  expect_length(index$metadata, 3)
-  expect_setequal(names(index$metadata), ids)
-  expect_mapequal(index$metadata, root_upstream$index_update()$metadata)
-
-  expect_s3_class(index$location, "data.frame")
-  expect_setequal(index$location$id, ids)
-  expect_equal(index$location$location, rep("upstream", 3))
+  outpack_init(path)
+  loc <- outpack_location_path$new(path)
+  expect_s3_class(loc, "outpack_location_path")
+  expect_null(loc$list())
 })
 
 
-test_that("pull metadata from subset of locations", {
-  create_packet <- function(root) {
-    src <- fs::dir_create(tempfile())
-    on.exit(unlink(src, recursive = TRUE))
-    saveRDS(runif(10), file.path(src, "data.rds"))
-    id <- outpack_packet_start(src, "data", root = root)$id
-    outpack_packet_end()
-    id
-  }
+test_that("outpack_location_path requires exact root", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
 
-  tmp <- tempfile()
-  on.exit(unlink(tmp, recursive = TRUE))
-  path <- root <- list()
-  path$a <- file.path(tmp, "a")
-  outpack_init(path$a, use_file_store = TRUE)
-  for (name in c("x", "y", "z")) {
-    path[[name]] <- file.path(tmp, name)
-    root[[name]] <- outpack_init(path[[name]], use_file_store = TRUE)
-    outpack_location_add(name, path[[name]], root = path$a)
-  }
+  expect_error(
+    outpack_location_path$new(path),
+    "File does not exist:")
 
-  expect_equal(outpack_location_list(root = path$a),
-               c("local", "x", "y", "z"))
+  outpack_init(path)
+  subdir <- file.path(path, "subdir")
+  dir.create(subdir)
+  expect_error(
+    outpack_location_path$new(subdir),
+    "'.+subdir' does not look like an outpack root")
 
-  ## NOTE: This is a little slow (0.2s) with about half of that coming
-  ## from the call to utils::sessionInfo which gets bogged down
-  ## reading DESCRIPTION files from disk - we might be better off
-  ## replacing that with something a bit simpler. Also seeing some
-  ## bottlenecks coming potentially from fs (fs::dir_create - looks
-  ## like a known bug)
-  ids <- list()
-  for (name in c("x", "y", "z")) {
-    ids[[name]] <- vcapply(1:3, function(i) create_packet(root[[name]]))
-  }
+  expect_silent(outpack_location_path$new(path))
+})
 
-  outpack_location_pull_metadata(c("x", "y"), root = path$a)
-  index <- outpack_root_open(path$a)$index_update()
-  expect_setequal(names(index$metadata), c(ids$x, ids$y))
-  expect_equal(index$location$location, rep(c("x", "y"), each = 3))
-  expect_equal(index$metadata[ids$x],
-               outpack_root_open(path$x)$index_update()$metadata)
-  expect_equal(index$metadata[ids$y],
-               outpack_root_open(path$y)$index_update()$metadata)
 
-  outpack_location_pull_metadata(root = path$a)
-  index <- outpack_root_open(path$a)$index_update()
-  expect_setequal(names(index$metadata), c(ids$x, ids$y, ids$z))
-  expect_equal(index$location$location, rep(c("x", "y", "z"), each = 3))
-  expect_equal(index$metadata[ids$z],
-               outpack_root_open(path$z)$index_update()$metadata)
+test_that("outpack_location_path returns list of ids", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+
+  outpack_init(path)
+  loc <- outpack_location_path$new(path)
+
+  ids <- vcapply(1:3, function(i) create_random_packet(path))
+
+  dat <- loc$list()
+  expect_s3_class(dat, "data.frame")
+  expect_equal(dat$id, ids)
+  expect_s3_class(dat$time, "POSIXt")
+  str <- vcapply(file.path(path, ".outpack", "metadata", ids), read_string)
+  expect_equal(
+    dat$hash,
+    vcapply(str, hash_data, "sha256", USE.NAMES = FALSE))
+})
+
+
+test_that("outpack_location_path can return metadata", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+
+  outpack_init(path)
+  loc <- outpack_location_path$new(path)
+
+  ids <- vcapply(1:3, function(i) create_random_packet(path))
+  str <- setNames(
+    vcapply(file.path(path, ".outpack", "metadata", ids), read_string),
+    ids)
+
+  expect_equal(loc$metadata(ids[[2]]), str[2])
+  expect_equal(loc$metadata(ids), str)
+  expect_equal(loc$metadata(rep(ids[[1]], 2)), str[c(1, 1)])
+})
+
+
+test_that("requesting nonexistant metadata is an error", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+
+  outpack_init(path)
+  loc <- outpack_location_path$new(path)
+  ids <- vcapply(1:3, function(i) create_random_packet(path))
+
+  errs <- c("20220317-125935-ee5fd50e", "20220317-130038-48ffb8ba")
+  expect_error(loc$metadata(errs[[1]]),
+               "Some ids not found: '20220317-125935-ee5fd50e'")
+  expect_error(
+    loc$metadata(errs),
+    "Some ids not found: '20220317-125935-ee5fd50e', '20220317") # truncated
+
+  expect_error(loc$metadata(c(ids[[1]], errs[[1]], ids[[2]])),
+               "Some ids not found: '20220317-125935-ee5fd50e'")
 })
--- tests/testthat/test-location.R
+++ tests/testthat/test-location.R
@@ -74,3 +74,120 @@ test_that("Require that (for now) locations must be paths", {
     outpack_location_add("other", other, root = path),
     "'.+' does not look like an outpack root")
 })
+
+
+test_that("can pull metadata from a file base location", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE))
+
+  path_upstream <- file.path(tmp, "upstream")
+  root_upstream <- outpack_init(path_upstream, use_file_store = TRUE)
+
+  ids <- vcapply(1:3, function(i) create_random_packet(path_upstream))
+
+  path_downstream <- file.path(tmp, "downstream")
+  outpack_init(path_downstream, use_file_store = TRUE)
+
+  outpack_location_add("upstream", path_upstream, root = path_downstream)
+  expect_equal(outpack_location_list(root = path_downstream),
+               c("local", "upstream"))
+
+  outpack_location_pull_metadata("upstream", root = path_downstream)
+
+  ## Sensible tests here will be much easier to write once we have a
+  ## decent query interface.
+  index <- outpack_root_open(path_downstream)$index_update()
+  expect_length(index$metadata, 3)
+  expect_setequal(names(index$metadata), ids)
+  expect_mapequal(index$metadata, root_upstream$index_update()$metadata)
+
+  expect_s3_class(index$location, "data.frame")
+  expect_setequal(index$location$id, ids)
+  expect_equal(index$location$location, rep("upstream", 3))
+})
+
+
+test_that("can pull empty metadata", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE))
+
+  path_upstream <- file.path(tmp, "upstream")
+  root_upstream <- outpack_init(path_upstream, use_file_store = TRUE)
+
+  path_downstream <- file.path(tmp, "downstream")
+  outpack_init(path_downstream, use_file_store = TRUE)
+
+  outpack_location_add("upstream", path_upstream, root = path_downstream)
+  outpack_location_pull_metadata("upstream", root = path_downstream)
+
+  index <- outpack_root_open(path_downstream)$index_update()
+  expect_length(index$metadata, 0)
+  ## This is what we need to improve, everywhere
+  expect_s3_class(index$location, "data.frame")
+})
+
+
+
+test_that("pull metadata from subset of locations", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE))
+  path <- root <- list()
+  path$a <- file.path(tmp, "a")
+  outpack_init(path$a, use_file_store = TRUE)
+  for (name in c("x", "y", "z")) {
+    path[[name]] <- file.path(tmp, name)
+    root[[name]] <- outpack_init(path[[name]], use_file_store = TRUE)
+    outpack_location_add(name, path[[name]], root = path$a)
+  }
+
+  expect_equal(outpack_location_list(root = path$a),
+               c("local", "x", "y", "z"))
+
+  ## NOTE: This is a little slow (0.2s) with about half of that coming
+  ## from the call to utils::sessionInfo which gets bogged down
+  ## reading DESCRIPTION files from disk - we might be better off
+  ## replacing that with something a bit simpler. Also seeing some
+  ## bottlenecks coming potentially from fs (fs::dir_create - looks
+  ## like a known bug)
+  ids <- list()
+  for (name in c("x", "y", "z")) {
+    ids[[name]] <- vcapply(1:3, function(i) create_random_packet(root[[name]]))
+  }
+
+  outpack_location_pull_metadata(c("x", "y"), root = path$a)
+  index <- outpack_root_open(path$a)$index_update()
+  expect_setequal(names(index$metadata), c(ids$x, ids$y))
+  expect_equal(index$location$location, rep(c("x", "y"), each = 3))
+  expect_equal(index$metadata[ids$x],
+               outpack_root_open(path$x)$index_update()$metadata)
+  expect_equal(index$metadata[ids$y],
+               outpack_root_open(path$y)$index_update()$metadata)
+
+  outpack_location_pull_metadata(root = path$a)
+  index <- outpack_root_open(path$a)$index_update()
+  expect_setequal(names(index$metadata), c(ids$x, ids$y, ids$z))
+  expect_equal(index$location$location, rep(c("x", "y", "z"), each = 3))
+  expect_equal(index$metadata[ids$z],
+               outpack_root_open(path$z)$index_update()$metadata)
+})
+
+
+test_that("Can't pull metadata from an unknown location", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+
+  outpack_init(path)
+  expect_error(
+    outpack_location_pull_metadata("upstream", root = path),
+    "Unknown location 'upstream'")
+})
+
+
+test_that("No-op to pull metadata from no locations", {
+  path <- tempfile()
+  on.exit(unlink(path, recursive = TRUE))
+
+  outpack_init(path)
+  expect_silent(outpack_location_pull_metadata("local", root = path))
+  expect_silent(outpack_location_pull_metadata(root = path))
+})
