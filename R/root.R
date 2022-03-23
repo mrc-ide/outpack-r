7ea2af82e1a988a23438a42f6bf74fc613b9ca32
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-03-23 11:51:58 +0000

    No longer return invisibly

--- R/root.R
+++ R/root.R
@@ -86,7 +86,7 @@ outpack_root <- R6::R6Class(
     index = function(refresh = FALSE) {
       prev <- if (refresh) list() else private$index_data
       private$index_data <- index_update(self, prev)
-      invisible(private$index_data)
+      private$index_data
     }
   ))
 
