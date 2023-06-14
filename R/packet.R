404cb0e53363c0b9e4129fa960d7c2ab9a80d4ee
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-06-14 08:52:08 +0100

    Fix test

--- R/packet.R
+++ R/packet.R
@@ -283,7 +283,7 @@ outpack_packet_use_dependency <- function(packet, query, files,
   }
 
   needs_pull <- search_options$allow_remote &&
-    root$config$core$require_complete_tree &&
+    packet$root$config$core$require_complete_tree &&
     !(id %in% packet$root$index()$unpacked$packet)
   if (needs_pull) {
     outpack::outpack_location_pull_packet(id, search_options$location,
